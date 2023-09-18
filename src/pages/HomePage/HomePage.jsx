ce3f4d8de2cfda2a67fa5825e90d9422135e7c2c
Inna Dmytrenko <inna.shalun@gmail.com>
2023-09-18 08:20:55 +0300

    fixed deploy config

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,10 @@
+import { ROUTER_URL_LIST } from '../../constants/index.js';
+import { Link } from 'react-router-dom';
+
 export const HomePage = () => {
-  return <h1>Home page</h1>;
+  return (
+    <h1>
+      Home page <Link to={ROUTER_URL_LIST.CATALOG}>to catalog</Link>
+    </h1>
+  );
 };
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://innuska23.github.io/goit-tech-part/',
+  base: '/goit-tech-part/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
